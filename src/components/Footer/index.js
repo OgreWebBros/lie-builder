2888a095fbcd5a0a8aae94e1094044305780822a
proach720 <proach720@comcast.com>
2021-08-05 18:18:13 -0400

    lotsa stuff

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,16 @@
 import { NavLink } from 'react-router-dom';
+import GhLogo from "../logo/gh-logo";
 
 const Footer = () => (
+  <div className="footer__wrapper">
     <div className="footer">
-        <NavLink 
-          className="footer__link button" exact activeClassName="active" to="/">Get a Lie</NavLink>
-        <NavLink className="footer__link button" activeClassName="active"  to="/Liebrary">Checkout the Liebrary</NavLink>
+      <a href="https://github.com/OgreWebBros/lie-builder">
+        <GhLogo block="footer" size={{ height: 40, width: 41 }} />
+      </a>
+      <NavLink
+        className="footer__link button" exact activeClassName="active" to="/">Get a Lie</NavLink>
+      <NavLink className="footer__link button" activeClassName="active" to="/Liebrary">Checkout the Liebrary</NavLink>
     </div>
+  </div>
 )
 export default Footer
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
 import { NavLink } from 'react-router-dom';
-import Logo from "../logo"
+import Logo from "../logo";
+
 const Header = () => (
-    <div className="header">
-        <NavLink
-            className="header__link " to="/">
-            <Logo block="header" size="40" />
-            <h2 className="header__title">Morgan's Trusty Lie Builder</h2>
-        </NavLink>
+    <div className="header__wrapper">
+        <div className="header">
+            <NavLink
+                className="header__link " to="/">
+                <Logo block="header" size="50" />
+                <h2 className="header__title">Lie Builder</h2>
+            </NavLink>
+        </div>
     </div>
 )
 export default Header
